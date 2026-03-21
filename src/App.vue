af68232d2ed501976f6e8a8818370e0e3f6f5bcd
vishalgautam78 <vishalgautam78@gmail.com>
2026-03-22 01:11:19 +0530

    worked on student page

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,33 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <header>
     <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
     <div class="wrapper">
       <!-- <HelloWorld msg="You did it!" /> -->
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" to="/">Logo</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link active" to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/about">About Us</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" to="/students">Student</RouterLink>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
-
 </style>
